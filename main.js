aa27d7b1e82acc1d1355014b753f4d276b92a1be
lucybrewton1 <76742499+lucybrewton1@users.noreply.github.com>
2021-11-28 14:31:40 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//https://teachablemachine.withgoogle.com/models/jAKKuGcYU/model.json
+function startRecognition() {
+    navigator.mediaDevices.getUserMedia({
+        audio:true 
+    });
+    classifier= ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/jAKKuGcYU/model.json",modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    console.log("got results");
+}
